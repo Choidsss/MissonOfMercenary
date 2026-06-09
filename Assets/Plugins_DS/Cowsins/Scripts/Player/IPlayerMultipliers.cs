2b7c9bd5789ed807f1858ed7245780bb0e87d8d2
Choidsss <133637761+Choidsss@users.noreply.github.com>
2026-06-09 17:36:01 +0900

    20260609_Commit

--- Assets/Plugins_DS/Cowsins/Scripts/Player/IPlayerMultipliers.cs
+++
@@ -1,9 +0,0 @@
-namespace cowsins
-{
-    public interface IPlayerMultipliers
-    {
-        float DamageMultiplier { get; set; }
-        float HealMultiplier { get; set; }
-        float WeightMultiplier { get; set; }
-    }
-}
