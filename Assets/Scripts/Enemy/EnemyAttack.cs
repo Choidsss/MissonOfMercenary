b115afbe55ae4a7d301a2f26b19887e91104c440
Choidsss <133637761+Choidsss@users.noreply.github.com>
2026-08-03 19:34:28 +0900

    20260803_Commit

--- Assets/Scripts/Enemy/EnemyAttack.cs
+++ Assets/Scripts/Enemy/EnemyAttack.cs
@@ -4,6 +4,10 @@ namespace MIssionOfMercenary
 {
     public class EnemyAttack : MonoBehaviour
     {
+        [SerializeField] GameObject _ammo;
+        [SerializeField] GameObject _muzzle;
+        [SerializeField] GameObject _muzzleFire;
+
         public AimType aimType { get; } = AimType.None;
 
         public WeaponType weaponType { get; set; } = WeaponType.AR;
@@ -12,14 +16,32 @@ namespace MIssionOfMercenary
 
         public float AttackRange { get; } = 150;
 
+
         void Start()
         {
             
         }
 
-        void Update()
+        private void Update()
         {
-        
+            
         }
+
+        void EnemyAttackOnPlayer()
+        {
+
+        }
+
+        void CalculateShotArea()
+        {
+            //이거 계산 하려면 어떻게 하지?
+        }
+
+
+        //정면으로 특정 넓이의 도형을 하나 만들어주는 함수를 만듦(이 안에서 랜덤하게 지점을 가져올것)
+        //적이 투사체를 쏘는 함수를 하나 만듦(위의 지점을 향해 발사)
+        //맞았는지 체크는 플레이어에서 투사체 체크하면 되고
+        //머즐에서 불빛 나가게 만드는 함수도 만들기
+        //계속 나가게 하면 안되므로, 3,4,5 발의 숫자중 랜덤하게 발사하도록 만드는 함수
     }
 }
--- Assets/Scripts/Enemy/EnemyAttackNode.cs
+++ Assets/Scripts/Enemy/EnemyAttackNode.cs
@@ -16,15 +16,9 @@ namespace MIssionOfMercenary
         public override State Evaluate()
         {
             if(_enemyAttack == null || !_enemyAttack.enabled || _findArea == null) { return State.Failure; }
-
             Transform target = _findArea.DetectedTarget;
 
             if(target == null || !_findArea.IsDetectedPlayer) { return State.Failure; }
-
-            //EnemyAttack에서 공격범위안에 없으면 실패\
-
-            //공격가능 리턴
-
 
             return State.Running;
         }
