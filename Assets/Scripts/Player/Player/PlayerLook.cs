2585fce4a12db18725fb39d6d7ad17a613ce2d22
Choidsss <133637761+Choidsss@users.noreply.github.com>
2026-09-04 18:54:30 +0900

    20260904_Commit

--- Assets/Scripts/Player/Player/PlayerLook.cs
+++ Assets/Scripts/Player/Player/PlayerLook.cs
@@ -61,36 +61,15 @@ namespace MIssionOfMercenary
             _lookAngle = Vector2.zero;
         }
 
-        //마우스 회전 담당 함수 
         void DoLook()
         {
-            //currentPitch에 Value.Y 값 누적시킴(Inverty는 위아래의 마우스 상하반전)
             _currentPitch += IsInverty ? _lookAngle.y : -_lookAngle.y;
-            //일정 각도 이상으로 돌리지 안도록 제한
-            _currentPitch = Mathf.Clamp(_currentPitch, -80, 80); // => 상하 회전의 누적 움직임 값
 
-            _yRotation = _lookAngle.x * _cameraSpeed; // FixedUpdate에서 쓸 값 저장
+            _currentPitch = Mathf.Clamp(_currentPitch, -80, 80); //상하 회전의 누적 움직임 값
+
+            _yRotation = _lookAngle.x * _cameraSpeed;
 
             _cameraTransform.localEulerAngles = new Vector3(_currentPitch * _cameraPitchSpeed, 0, 0);
-            
-            
-            //카메라 좌우회전에 X축 을 생성해줌(더해줌)
-
-            //Quaternion rotationY = Quaternion.Euler(0, _lookAngle.x * _cameraSpeed, 0);
-            //_rigidBody.MoveRotation(_rigidBody.rotation * rotationY);
-            //transform.Rotate(0, _lookAngle.x * _cameraSpeed, 0);
-
-            //플레이어의 몸통을 카메라가 돌아간 것 만큼 돌림(Y축 각도 + 카메라의 좌우 각도)
-            //=> 좌우는 그냥 돌리면 되지만 Y축은 누적된만큼 돌려야 함
-
-            //this.transform.eulerAngles = new Vector3(0, this.transform.eulerAngles.y + _lookAngle.x, 0);
-
-
-
-            //_cameraTransform.eulerAngles = new Vector3(_currentPitch, 0, 0);
-
-
-            //transform.Rotate(0, _lookAngle.x, 0);
         }
 
 
